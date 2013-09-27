e1ea11181871157695156b87eb6be2c73a4d9b01
Robert Stoll <rstoll@tutteli.ch>
2013-09-27 10:56:06 +0200

    TSPHP-584 remove copyright and author block from files

--- src/ch/tutteli/tsphp/HardCodedCompilerInitialiser.java
+++ src/ch/tutteli/tsphp/HardCodedCompilerInitialiser.java
@@ -1,19 +1,3 @@
-/*
- * Copyright 2013 Robert Stoll <rstoll@tutteli.ch>
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- * http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * 
- */
 package ch.tutteli.tsphp;
 
 import ch.tutteli.tsphp.common.ICompiler;
@@ -26,10 +10,6 @@ import ch.tutteli.tsphp.typechecker.TypeChecker;
 import java.util.ArrayDeque;
 import java.util.Collection;
 
-/**
- *
- * @author Robert Stoll <rstoll@tutteli.ch>
- */
 public class HardCodedCompilerInitialiser implements ICompilerInitialiser
 {
 
@@ -42,7 +22,7 @@ public class HardCodedCompilerInitialiser implements ICompilerInitialiser
 
     @Override
     public ICompiler create(final int numberOfWorkers) {
-        Collection<ITranslatorFactory> translatorFactories = new ArrayDeque();
+        Collection<ITranslatorFactory> translatorFactories = new ArrayDeque<>();
         translatorFactories.add(new PHP54TranslatorFactory());
 
         ITSPHPAstAdaptor adaptor = new TSPHPAstAdaptor();
